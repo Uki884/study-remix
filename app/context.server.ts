69448577d80fee456232730f3f35d3ce3096ef95
Yuki Hayashi <mrchildreny@gmail.com>
2024-09-17 10:49:49 +0900

    fix

--- app/context.server.ts
+++ app/context.server.ts
@@ -2,6 +2,7 @@ import { chromium } from 'playwright';
 import { singleton } from './singleton.server'
 
 const browser = await chromium.launch({ headless: true });
+console.log('Browser launched');
 
 const getPageById = async (id: string) => {
   const context = await singleton('context', () => browser.newContext(), id);
